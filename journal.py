7febacad3e5f9bb4a1575f17a592cd32c5ccaa23
Karen Wong <karen.wsywong@gmail.com>
2015-06-26 15:33:58 -0700

    add css

--- journal.py
+++ journal.py
@@ -15,6 +15,7 @@ from sqlalchemy.exc import DBAPIError
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
 import datetime
+HERE = os.path.dirname(os.path.abspath(__file__))
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
@@ -106,6 +107,7 @@ def main():
     )
     config.include('pyramid_tm')
     config.include('pyramid_jinja2')
+    config.add_static_view('static', os.path.join(HERE, 'static'))
     config.add_route('home', '/')
     config.add_route('add', '/add')
     config.add_route('login', '/login')
